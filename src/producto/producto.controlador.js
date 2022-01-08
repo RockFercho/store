c53f53cc19fe29f596ebc47ff96d62fa0106b108
Fernando Hurtado <RockFercho@gmail.com>
2022-01-08 00:10:18 -0400

    dia3 _getById/getquery tarea put delete

--- src/producto/producto.controlador.js
+++ src/producto/producto.controlador.js
@@ -1,23 +1,78 @@
 'use strict'
 
 const PRODUCT = [
-  { nombre: 'coco cola', precio: 13, vencimiento: null },
-  { nombre: 'fanta', precio: 13, vencimiento: null },
-  { nombre: 'macarron', precio: 13, vencimiento: null }
+  { id: 1, nombre: 'coco cola', precio: 13, vencimiento: null },
+  { id: 2, nombre: 'fanta', precio: 13, vencimiento: null },
+  { id: 3, nombre: 'macarron', precio: 13, vencimiento: null }
 ];
 
 function getProducto(req, res) {
+  if(req.query.nombre) {
+    const result = buscar(req.query);
+    if(result === null) {
+      return res.status(404).json(result);
+    }
+    return res.status(200).json(result);
+  }
   return res.status(200).json(PRODUCT);
+}
+function buscar(dato){
+  let save = null;
+  PRODUCT.forEach(function (producto) {
+    if(producto.nombre == dato.nombre) {
+      save = producto;
+    }
+  });
+  
+  return save;
+}
+
+function buscarId(id){
+  let save = null;
+  PRODUCT.forEach(function (producto) {
+    if(producto.id == id) {
+      save = producto;
+    }
+  });
+  
+  return save;
+}
+
+function getById(req, res) {
+  const id = req.params.id;
+  const result = buscarId(id);
+  if(result === null) {
+    return res.status(404).json(result);
+  }
+  return res.status(200).json(result);
 }
 
 function guardarProducto(req, res) {
   let producto_guardar = req.body;
   console.log(req.body);
   PRODUCT.push(producto_guardar);
-  return res.status(200).json(PRODUCT);
+  return res.status(200).json(producto_guardar);
+}
+
+function actualizar(req, res) {
+  const id = req.params.id;
+  const datoActualizar = req.body;
+
+  const result = buscarId(id);
+  if(result === null) {
+    return res.status(404).json(result);
+  }
+  return res.status(200).json(result);
+}
+
+function eliminar(req, res) {
+  //tu logica
 }
 
 module.exports = {
   getProducto,
-  guardarProducto
+  guardarProducto,
+  getById,
+  actualizar,
+  eliminar
 }
--- src/ruta/producto.js
+++ src/ruta/producto.js
@@ -5,6 +5,9 @@ const ROUTER = express.Router();
 
 ROUTER
   .get('/', producto.getProducto)
+  .get('/:id', producto.getById)
+  .put('/:id', producto.actualizar)
+  .delete('/:id', producto.eliminar)
   .post('/', producto.guardarProducto);
 
 module.exports = ROUTER;
